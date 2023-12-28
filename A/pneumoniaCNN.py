2ce4f377cff1cf01562b8918796d2dcf0a4c4d2c
leonardo-p <leonardo.phithak@gmail.com>
2023-12-28 17:19:14 +0200

    Refactored Task 1 code

--- A/pneumoniaCNN.py
+++ A/pneumoniaCNN.py
@@ -2,41 +2,34 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import accuracy_score
 
 class PneumoniaCNNClassifier:
-    def __init__(self, dataset_path, use_cross_validation, use_bagging, folds = None):
-        self.dataset_path = dataset_path
-        self.folds = folds
-        self.use_cross_validation = use_cross_validation
-        self.use_bagging = use_bagging
-        self.models = []
-        self.load_dataset()
+    def __init__(self, dataset_path):
+        self.load_dataset(dataset_path)
 
-    # Load the dataset and concatenate training and validation data
-    def load_dataset(self):
-        pneumonia_dataset = np.load(self.dataset_path)
+    # Method to load the dataset and separate the training, validation and testing images/labels
+    def load_dataset(self, dataset_path):
+        pneumonia_dataset = np.load(dataset_path)
 
-        # Training images and labels
+        # Create variables for the normalised images
         self.training_images = self.normalize_images(pneumonia_dataset['train_images'])
-        self.training_labels = pneumonia_dataset['train_labels']
-
-        # Validation images and labels
         self.validation_images = self.normalize_images(pneumonia_dataset['val_images'])
-        self.validation_labels = pneumonia_dataset['val_labels']
-
-        # Testing images and labels
         self.testing_images = self.normalize_images(pneumonia_dataset['test_images'])
+
+        # Create variables for the labels
+        self.training_labels = pneumonia_dataset['train_labels']
+        self.validation_labels = pneumonia_dataset['val_labels']
         self.testing_labels = pneumonia_dataset['test_labels']
 
+        # Extract the image height, width and number of channels to be used in the CNN
         self.image_height, self.image_width = self.training_images[0].shape
         self.number_of_channels = 1  # Because they are grayscale images
 
-    # Normalize images to have value between 0 and 1
+    # Method to normalize images to have value between 0 and 1
     def normalize_images(self, images):
         return images.astype('float32') / 255.0
 
-    # Build CNN model and add all the layers
+    # Method to build CNN model and add all the layers
     def build_model(self):
         model = Sequential()
         model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(self.image_height, self.image_width, self.number_of_channels)))
@@ -47,92 +40,69 @@ class PneumoniaCNNClassifier:
         model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
         return model
 
-    # Train the CNN model on the combined training and validation data and validate it on the cross-validated folds
-    def train_and_test_model(self, epochs, batch_size):
-        if self.use_cross_validation:
-            self.train_with_cross_validation(epochs, batch_size)
-        else:
-            self.train_without_cross_validation(epochs, batch_size)
-
-    # Train the CNN model without cross-validation
+    # Method to train the CNN model without cross-validation
     def train_without_cross_validation(self, epochs, batch_size):
         print("Training without cross-validation")
+
+        # Build model and train it with training and validation datasets
         self.model = self.build_model()
         self.model.fit(
-            self.training_images, self.training_labels,
-            epochs=epochs, batch_size=batch_size,
+            self.training_images,
+            self.training_labels,
+            epochs=epochs,
+            batch_size=batch_size,
             validation_data=(self.validation_images, self.validation_labels)
         )
 
-    # Train the CNN model with cross-validation
-    def train_with_cross_validation(self, epochs, batch_size):
+        # Evaluate the model through the test accuracy
+        self.test_model(used_cross_validation=False)
+
+    # Method to train the CNN model with cross-validation
+    def train_with_cross_validation(self, epochs, batch_size, folds):
         print("Training with cross-validation")
-        cross_validation = StratifiedKFold(n_splits=self.folds, shuffle=True)
+
+        # To store all the models for each fold
+        self.models = []
+
+        # Prepare cross validation folds with shuffling
+        cross_validation = StratifiedKFold(n_splits=folds, shuffle=False)
         cross_validation_folds = cross_validation.split(self.training_images, self.training_labels)
 
-        # In the tuple (train_index, _), we don't use the val_index because every fold validates on the same validation dataset provided
-        for fold, (train_index, _) in enumerate(cross_validation_folds):
-            print(f"Training on fold {fold + 1}/{self.folds}")
+        # In the tuple (train_index, val_index), we don't use the val_index because every fold uses the same validation dataset
+        for fold, (train_index, val_index) in enumerate(cross_validation_folds):
+            print(f"Training on fold {fold + 1}/{folds}")
             current_fold_images = self.training_images[train_index]
             current_fold_labels = self.training_labels[train_index]
+
+            # Train model on each individual fold (using the same validation data), and append it to the models array
             self.model = self.build_model()
             self.model.fit(
-                current_fold_images, current_fold_labels,
-                epochs=epochs, batch_size=batch_size,
+                current_fold_images,
+                current_fold_labels,
+                epochs=epochs,
+                batch_size=batch_size,
                 validation_data=(self.validation_images, self.validation_labels)
             )
-
             self.models.append(self.model)
 
-        if self.use_bagging:
-            self.evaluate_model_with_bagging()
-        else:
-            self.evaluate_model()
+        # Evaluate the model through the test accuracy
+        self.test_model(used_cross_validation=True)
+
 
     # Evaluate the loss and the accuracy of the trained CNN model
-    def evaluate_model(self):
-        if self.use_cross_validation:
+    def test_model(self, used_cross_validation):
+        # Print accuracies of all models if cross validation was used
+        if used_cross_validation:
             for fold, model in enumerate(self.models):
                 test_loss, test_accuracy = model.evaluate(self.testing_images, self.testing_labels, verbose=0)
-                print(f'Fold {fold + 1} - Test accuracy: {100 * test_accuracy:.2f}%')
+                print(f'Fold {fold + 1} - Test Loss: {test_loss:.4f}, Test accuracy: {100 * test_accuracy:.2f}%')
+        # Else print accuracy of just the one model if cross validation was not used
         else:
-            test_loss, test_accuracy = self.model.evaluate(self.testing_images, self.testing_labels)
-            print(f'Test accuracy: {100 * test_accuracy:.2f}%')
-
-    # Method to perform bagging and aggregate predictions
-    def bagging(self, test_data):
-        # Aggregate predictions from each model
-        predictions = np.zeros((len(test_data),))
-        for model in self.models:
-            model_predictions = model.predict(test_data)
-            predictions += model_predictions.flatten()
-
-        # Take the majority vote as the final prediction
-        predictions = (predictions >= (len(self.models) / 2)).astype(int)
-
-        return predictions
-
-    # Evaluate the total accuracy of the trained CNN model using bagging
-    def evaluate_model_with_bagging(self):
-        # Display test accuracy of each CV fold
-        for fold, model in enumerate(self.models):
-            test_predictions = model.predict(self.testing_images)
-            test_accuracy = accuracy_score(self.testing_labels, (test_predictions >= 0.5).astype(int))
-            print(f'Fold {fold + 1} - Test accuracy: {100 * test_accuracy:.2f}%')
-
-        # Display test accuracy with bagging
-        bagged_predictions = self.bagging(self.testing_images)
-        total_accuracy = accuracy_score(self.testing_labels, bagged_predictions)
-        print(f'Total accuracy with bagging: {100 * total_accuracy:.2f}%')
-
+            test_loss, test_accuracy = self.model.evaluate(self.testing_images, self.testing_labels, verbose=0)
+            print(f'Test loss: {test_loss:.4f}, Test accuracy: {100 * test_accuracy:.2f}%')
 
 
 if __name__ == "__main__":
     # Example usage with cross-validation
-    classifier_with_cross_val = PneumoniaCNNClassifier(
-        '../Datasets/pneumoniamnist.npz', 
-        use_cross_validation = True, 
-        use_bagging = False,
-        folds = 10
-        )
-    classifier_with_cross_val.train_and_test_model(20, 32)
+    classifier_with_cross_val = PneumoniaCNNClassifier('./Datasets/pneumoniamnist.npz')
+    classifier_with_cross_val.train_with_cross_validation(epochs=20, batch_size=32, folds=10)
--- A/pneumoniaSVM.py
+++ A/pneumoniaSVM.py
@@ -1,39 +1,38 @@
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
-from skimage import feature
 import matplotlib.pyplot as plt
 
 class PneumoniaSVMClassifier:
     def __init__(self, dataset_path):
-        self.dataset_path = dataset_path
-        self.load_dataset()
+        self.load_dataset(dataset_path)
 
-    # Load the dataset and separate the training, validation and testing images/labels
-    def load_dataset(self):
-        pneumonia_dataset = np.load(self.dataset_path)
+    # Method to load the dataset and separate the training, validation and testing images/labels
+    def load_dataset(self, dataset_path):
+        pneumonia_dataset = np.load(dataset_path)
+
+        # Create variables for the images
         self.training_images = pneumonia_dataset['train_images']
         self.validation_images = pneumonia_dataset['val_images']
         self.testing_images = pneumonia_dataset['test_images']
 
+        # Create variables for the labels
         self.training_labels = pneumonia_dataset['train_labels']
         self.validation_labels = pneumonia_dataset['val_labels']
         self.testing_labels = pneumonia_dataset['test_labels']
 
+    # Method to extract Pixel Values and Histograms as features from an array of images
     def extract_features(self, images):
-        # Flatten pixel values
+        # Flatten pixel values for each image
         pixel_values = images.reshape(images.shape[0], -1)
 
-        # Extract Histograms as features
+        # Extract Histograms
         histograms = np.array([np.histogram(image.flatten(), bins=256, range=[0, 256])[0] for image in images])
 
-        # Extract Local Binary Patterns (LBP) as features
-        lbps = np.array([feature.local_binary_pattern(image.astype(int), P=8, R=1, method="uniform").flatten() for image in images])
+        # Return combined features
+        return np.concatenate((pixel_values, histograms), axis=1)
 
-        # Combine features
-        features_combined = np.concatenate((pixel_values, histograms, lbps), axis=1)
-        return features_combined
-    
+    # Method to display a sample histogram for the report
     def display_sample_histogram(self):
         histograms = np.array([np.histogram(image.flatten(), bins=256, range=[0, 256])[0] for image in self.training_images])
         first_image_histogram = histograms[0]
@@ -44,43 +43,39 @@ class PneumoniaSVMClassifier:
         plt.ylabel('Frequency')
         plt.show()
 
+    # Method to train the SVM model
     def train_model(self):
-        # Extract features for training
-        training_data_combined = self.extract_features(self.training_images)
+        # Extract features from training images
+        training_data_features = self.extract_features(self.training_images)
 
-        # Train SVM
-        self.svm_model = SVC(kernel='linear', C=1.0)
-        self.svm_model.fit(training_data_combined, self.training_labels.ravel())
+        # Train the model
+        self.svm_model = SVC(kernel='linear')
+        self.svm_model.fit(training_data_features, self.training_labels.ravel())
 
-    def validate_model(self):
-        # Extract features for validation
-        validation_data_combined = self.extract_features(self.validation_images)
+    # Method to validate and test the SVM model
+    def validate_and_test_model(self):
+        # Extract features from validation and testing images
+        validation_data_features = self.extract_features(self.validation_images)
+        testing_data_features = self.extract_features(self.testing_images)
 
-        # Validate the model
-        validation_predictions = self.svm_model.predict(validation_data_combined)
+        # Get validation and testing predictions
+        validation_predictions = self.svm_model.predict(validation_data_features)
+        testing_predictons = self.svm_model.predict(testing_data_features)
 
         # Evaluate on validation set
         val_accuracy = accuracy_score(self.validation_labels, validation_predictions)
         print(f'Validation Accuracy: {100*val_accuracy:.2f}%')
 
-    def test_model(self):
-        # Extract features for testing
-        testing_data_combined = self.extract_features(self.testing_images)
-
-        # Test the model
-        testing_predictons = self.svm_model.predict(testing_data_combined)
-
         # Evaluate on test set
         test_accuracy = accuracy_score(self.testing_labels, testing_predictons)
         print(f'Test Accuracy: {100*test_accuracy:.2f}%')
 
-        # Classification report for test set
+        # Display classification report for test set
         print("\nClassification Report:")
         print(classification_report(self.testing_labels, testing_predictons))
 
 
 if __name__ == "__main__":
-    classifier = PneumoniaSVMClassifier('../Datasets/pneumoniamnist.npz')
+    classifier = PneumoniaSVMClassifier('./Datasets/pneumoniamnist.npz')
     classifier.train_model()
-    classifier.validate_model()
-    classifier.test_model()
+    classifier.validate_and_test_model()
